e34a8f9362104b0e687f4789de30b5e774e657b4
ruchi panchal <ruchimpanchal@gmail.com>
2024-05-30 16:13:01 +0530

    Ruchi

---
+++ Controllers/ruchiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinallLaibrary.Controllers
+{
+    public class ruchiController : Controller
+    {
+        // GET: ruchi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
